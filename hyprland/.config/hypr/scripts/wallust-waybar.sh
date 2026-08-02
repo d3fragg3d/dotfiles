bb5534fa99f7a6e325e4daa6e53ce8483dec8e0b
Chris King <chris@k1ng.sh>
2026-08-02 21:31:28 +0000

    Slight changes after clean install on another laptop

---
+++ hyprland/.config/hypr/scripts/wallust-waybar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source ~/.cache/wallust/current.sh || exit 0
+
+accent=$(
+    for hex in ${color0#\#} ${color1#\#} ${color2#\#} ${color3#\#} ${color4#\#} ${color5#\#} \
+               ${color6#\#} ${color7#\#} ${color8#\#} ${color9#\#} ${color10#\#} ${color11#\#} \
+               ${color12#\#} ${color13#\#} ${color14#\#} ${color15#\#}; do
+        r=$((16#${hex:0:2})); g=$((16#${hex:2:2})); b=$((16#${hex:4:2}))
+        max=$(( r > g ? (r > b ? r : b) : (g > b ? g : b) ))
+        min=$(( r < g ? (r < b ? r : b) : (g < b ? g : b) ))
+        printf '%05d #%s\n' "$(( max - min ))" "$hex"
+    done | sort -rn | head -1 | awk '{print $2}'
+)
+
+printf '@define-color accent %s;\n' "$accent" > ~/.config/waybar/colors.css
+
+pkill -SIGUSR2 waybar 2>/dev/null || true
